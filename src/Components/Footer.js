3a7a535ebb4ed8a18bed0d7008564cd59e8ce1a7
aramaisay <aramaisyeghiazaryan@gmail.com>
2020-12-30 14:04:33 +0400

    Footer topCont done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
+import '../Css/Components/Footer.css';
+
 const Footer = () => {
+
+    const data = {
+        socialLinks: [
+            {
+                socialNetwork: 'facebook',
+                link: 'https://www.facebook.com/',
+                imageUrl: 'https://seeklogo.com/images/F/facebook-icon-circle-logo-09F32F61FF-seeklogo.com.png'
+            },
+            {
+                socialNetwork: 'instagram',
+                link: 'https://www.instagram.com/',
+                imageUrl: 'https://www.catholiccharitiesdc.org/wp-content/uploads/2018/10/instagram-colourful-icon.png'
+            },
+            {
+                socialNetwork: 'twitter',
+                link: 'https://twitter.com/?lang=en',
+                imageUrl: 'https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png'
+            }
+        ]
+    }
+
     return(
-        <div>
-            Footer
+        <div className = 'footerWrapper' >
+            <div className = 'topCont' >
+                <div className = 'followUsCont'>
+                    <div className = 'followUsText' > Follow Us </div>
+                    <div className = 'icons' >
+                        {data.socialLinks.map((item, index) => {
+                                return <a className = 'linkExternal' target = '_blank' href = {item.link}><img className = 'linkImg' src = {item.imageUrl} alt = {item.socialNetwork}/></a>
+                        })}
+                    </div>                   
+                </div>
+                <div className = 'emailCollector' >
+                    <div className = 'emailText' >subscribe to our news letter</div>
+                    <input className = 'emailInput' placeholder = 'email' ></input>
+                    <div className = 'emailBtn' >send</div>
+                </div>
+                <div className = 'arrowUp' >
+                    <img className = 'arrowUpImg'  src = 'https://www.freeiconspng.com/uploads/black-circle-arrow-up-icon-31.png'/>
+                </div>
+            </div>
         </div>
     )
 }
